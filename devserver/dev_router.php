47fe86e9a3b511d698e971e0d7a3090398071f41
nralbrecht <nralbrecht@web.de>
2020-11-13 18:05:59 +0100

    Proxied API responses should reflect original response code

--- devserver/dev_router.php
+++ devserver/dev_router.php
@@ -2,7 +2,14 @@
 
 if (preg_match('/^\/api/', $_SERVER["REQUEST_URI"])) {
     header("Content-Type: application/json");
-    echo file_get_contents("https://aviationtools.de" . $_SERVER["REQUEST_URI"]);
+    $response = file_get_contents("https://aviationtools.de" . $_SERVER["REQUEST_URI"]);
+
+    $status_line = $http_response_header[0];
+    preg_match('{HTTP/\S*\s(\d{3})}', $status_line, $match);
+    $status = $match[1];
+    http_response_code($status);
+
+    echo $response;
 } else {
     return false; // serve the requested resource as-is.
 }
