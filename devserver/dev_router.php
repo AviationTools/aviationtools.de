4cca128c6e7f7b120c03b55a0bfa08db5c765978
nralbrecht <nralbrecht@web.de>
2020-11-15 20:36:54 +0100

    Use curl in devserver instead of file_get_contents

--- devserver/dev_router.php
+++ devserver/dev_router.php
@@ -1,15 +1,34 @@
 <?php
 
+$USE_CURL = true;
+
 if (preg_match('/^\/api/', $_SERVER["REQUEST_URI"])) {
-    header("Content-Type: application/json");
-    $response = file_get_contents("https://aviationtools.de" . $_SERVER["REQUEST_URI"]);
+    if ($USE_CURL) {
+        $ch = curl_init();
 
-    $status_line = $http_response_header[0];
-    preg_match('{HTTP/\S*\s(\d{3})}', $status_line, $match);
-    $status = $match[1];
-    http_response_code($status);
+        curl_setopt($ch, CURLOPT_URL, "https://aviationtools.de" . $_SERVER["REQUEST_URI"]);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 
-    echo $response;
+        $data = curl_exec($ch);
+        $response_code = curl_getinfo($ch, CURLINFO_RESPONSE_CODE);
+        curl_close($ch);
+
+        header("Content-Type: application/json");
+        http_response_code($response_code);
+        echo $data;
+    }
+    else {
+        $response = file_get_contents("https://aviationtools.de" . $_SERVER["REQUEST_URI"]);
+
+        $status_line = $http_response_header[0];
+        preg_match('{HTTP/\S*\s(\d{3})}', $status_line, $match);
+        $status = $match[1];
+
+        header("Content-Type: application/json");
+        http_response_code($status);
+        echo $response;
+    }
 } else {
     return false; // serve the requested resource as-is.
 }
