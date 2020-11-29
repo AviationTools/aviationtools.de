197e9b7b6b3c0ea187fc043cf45d5656de999b9b
AviationTools <werbungbnc@web.de>
2020-11-29 18:19:48 +0100

    New Leaflet implementation
    New Mapbox URL

--- sidstar/sidstar.js
+++ sidstar/sidstar.js
@@ -585,8 +585,8 @@ let mymap = L.map('mapid');
 initMap();
 
 function initMap() {
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>',
         maxZoom: 14,
         id: 'mapbox.streets',
         accessToken: 'pk.eyJ1IjoiYm5jNGsiLCJhIjoiY2pwcmhnNzF2MDFlejN4bjdjamR2cHJnNCJ9.mZMpSXrNgjTPU0dky1cfvA'
